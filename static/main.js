d43b3ad40403dbf09e9016beddaf110555b329d6
Bas1925 <basel.samhat.99@gmai.com>
2024-08-01 17:46:00 +0300

    first commit

---
+++ static/main.js
@@ -0,0 +1,34 @@
+// JavaScript for handling the toggle menu and scroll reveal animations
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // Animation repeat
+});
+
+sr.reveal(`.home__data, .about__data, .popular__card, .features__content, .featured__card, .offer__card, .logos__content, .contact__form`, {
+    interval: 100
+});
