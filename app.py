5e604cd2558a7670d229ac368e16e388b0ed6d50
Bas1925 <basel.samhat.99@gmai.com>
2024-08-22 18:15:17 +0300

    lets doit again

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/contact')
+@app.route('/contact' )
 def contact():
     return render_template('contact.html')
 
